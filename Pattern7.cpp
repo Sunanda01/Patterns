13c3bbd16cdff799f6e59884e1b2a4898190c07c
Sunanda Sadhukhan <sunandasadhukhan1@gmail.com>
2023-08-24 23:29:00 +0530

    Create Pattern7.cpp

---
+++ Pattern7.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main() {
+    int i,j,n,num=1;
+    cout<<"Enter The Limit = ";
+    cin>>n;
+    for(i=1;i<=n;i++)
+    {
+        for(j=1;j<=i;j++)
+        {
+            cout<<" "<<num;
+            num++;
+        }
+        cout<<"\n";
+    }
+
+    return 0;
+}
