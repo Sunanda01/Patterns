016d62c05f988559fab580e1621490fb1e5bfc82
Sunanda Sadhukhan <sunandasadhukhan1@gmail.com>
2023-08-24 23:43:18 +0530

    insert

---
+++ Pattern10.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int main() {
+    int i,j,n,space;
+    cout<<"Enter The Limit = ";
+    cin>>n;
+    for(i=n;i>=1;i--)
+    {
+        for(space=1;space<=(n-i);space++)
+        {
+            cout<<" ";
+        }
+        for(j=1;j<=2*i-1;j++)
+        {
+            cout<<"*";
+        }
+        cout<<"\n";
+    }
+
+    return 0;
+}
