541ebb6e1a26baf223e6c1881c7a38c2c6fddb25
Sunanda Sadhukhan <sunandasadhukhan1@gmail.com>
2023-08-24 23:38:09 +0530

    Create Pattern8.cpp

---
+++ Pattern8.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int main() {
+    int i,j,n,space;
+    cout<<"Enter The Limit = ";
+    cin>>n;
+    for(i=1;i<=n;i++)
+    {
+        for(space=1;space<=(n-i);space++)
+        {
+            cout<<" ";
+        }
+        for(j=1;j<=i;j++)
+        {
+            cout<<"* ";
+        }
+        cout<<"\n";
+    }
+
+    return 0;
+}5
