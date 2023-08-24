9f30d373832f35b9bf8deedcd8a21a1ac70db1c8
Sunanda Sadhukhan <sunandasadhukhan1@gmail.com>
2023-08-24 22:49:29 +0530

    Create Pattern2.cpp

---
+++ Pattern2.cpp
@@ -0,0 +1,18 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+
+int main() {
+    int i,j,n;
+    cout<<"Enter The Limit = ";
+    cin>>n;
+    for(i=n;i>=1;i--)
+    {
+        for(j=1;j<=i;j++)
+        {
+            cout<<"*";
+        }
+        cout<<"\n";
+    }
+
+    return 0;
+}
