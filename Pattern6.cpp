3ee2dac8356a15f4c319bd7d6ace6c3e757917d8
Sunanda Sadhukhan <sunandasadhukhan1@gmail.com>
2023-08-25 15:15:06 +0530

    Update Pattern6.cpp

--- Pattern6.cpp
+++ Pattern6.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-
+using namespace std;
 int main() {
     int i,j,n;
     cout<<"Enter The Limit = ";
@@ -12,6 +12,5 @@ int main() {
         }
         cout<<"\n";
     }
-
     return 0;
 }
